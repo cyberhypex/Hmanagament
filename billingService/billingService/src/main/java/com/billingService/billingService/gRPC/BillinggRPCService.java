bc21ef0b5628c9036b3aafe4f49fa2f62a0b11d0
Anshuman <chaoanxuman999@gmail.com>
2026-01-25 22:16:25 +0530

    gRPC communicating well

--- billingService/billingService/src/main/java/com/billingService/billingService/gRPC/BillinggRPCService.java
+++ billingService/billingService/src/main/java/com/billingService/billingService/gRPC/BillinggRPCService.java
@@ -1,11 +1,27 @@
 package com.billingService.billingService.gRPC;
 
 
+import billing.BillingResponse;
 import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class BillinggRPCService extends BillingServiceGrpc.BillingServiceImplBase
         {
+                private static final Logger log = LoggerFactory.getLogger(BillinggRPCService.class);
 
+                @Override
+                public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver){
+
+                        log.info("Billing Account creation request recieved {}",billingRequest.toString());
+
+                        //-> Business logic - e.g. save to db, perform calculation etc
+
+                        BillingResponse response= BillingResponse.newBuilder().setAccountId("12345").setStatus("ACTIVE").build();
+                        responseObserver.onNext(response);
+                        responseObserver.onCompleted();
+                }
 }
