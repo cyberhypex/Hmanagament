9b5de2c6658512be4b6a20c2849ca5f45c0aaf21
Anshuman <chaoanxuman999@gmail.com>
2026-01-25 11:14:05 +0530

    Fixed generated sources issue

---
+++ billingService/billingService/src/main/java/com/billingService/billingService/gRPC/BillinggRPCService.java
@@ -0,0 +1,11 @@
+package com.billingService.billingService.gRPC;
+
+
+import billing.BillingServiceGrpc;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillinggRPCService extends BillingServiceGrpc.BillingServiceImplBase
+        {
+
+}
